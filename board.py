fd2fc230f10432dda1d7765d7ad0d8ef4bb513e7
Kagaminara Shikatsu <misterarkandias@gmail.com>
2017-02-05 18:10:26 +0100

    Fixed board

--- board.py
+++ board.py
@@ -3,7 +3,7 @@ class Board:
 		self.board = [[None] * 16] * 16
 	
 	def __getitem__(self, key):
-		return self.board[piece.position['x']][piece.position['y']]
+		return self.board[key[0]][key[1]]
 
 	def __setitem__(self, key, value):
 		self.board[key[0]][key[1]] = value
