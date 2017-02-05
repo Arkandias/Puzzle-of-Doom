bb4efd9a4b3c2fa1b3345628356f4b4bc07e7936
Kagaminara Shikatsu <misterarkandias@gmail.com>
2017-02-05 18:12:47 +0100

    Converted tabs to spaces

--- board.py
+++ board.py
@@ -1,9 +1,9 @@
 class Board:
-	def __init__(self):
-		self.board = [[None] * 16] * 16
-	
-	def __getitem__(self, key):
-		return self.board[key[0]][key[1]]
+    def __init__(self):
+        self.board = [[None] * 16] * 16
+    
+    def __getitem__(self, key):
+        return self.board[key[0]][key[1]]
 
-	def __setitem__(self, key, value):
-		self.board[key[0]][key[1]] = value
+    def __setitem__(self, key, value):
+        self.board[key[0]][key[1]] = value
