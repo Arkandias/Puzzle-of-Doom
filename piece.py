5f7411c610fd038d84887ea4ee4a556c69e97f43
Kagaminara Shikatsu <misterarkandias@gmail.com>
2017-02-09 00:28:36 +0100

    Added getSidePattern method to get the pattern of a specific side.

--- piece.py
+++ piece.py
@@ -12,3 +12,13 @@ class Piece:
         self.position = {"x":None, "y":None}
         self.nbofrightrotate = 0
         self.imgpath = None
+
+    def getSidePattern(self, rotation):
+        rotation = rotation - self.nbofrightrotate if rotation - self.nbofrightrotate > 0 else rotation - self.nbofrightrotate + 4
+        def switch(x):
+            return {
+                0: self.upEdge,
+                1: self.rightEdge,
+                2: self.downEdge,
+                3: self.leftEdge
+            }.get(x, False)
