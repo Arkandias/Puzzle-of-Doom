94d38823c27a78a3bbd408cc953bf22f1906b61c
Kagaminara Shikatsu <misterarkandias@gmail.com>
2017-02-08 19:56:56 +0100

    Fixed board

--- board.py
+++ board.py
@@ -1,16 +1,17 @@
 class Board:
     def __init__(self):
-        self.board = [[None] * 16] * 16
+        self.board = [None] * 256
     
     def __getitem__(self, key):
-        return self.board[key[0]][key[1]]
+        return self.board[key[0] + key[1] * 16]
 
     def __setitem__(self, key, value):
+        print(key)
         value.position['x'] = key[0]
         value.position['y'] = key[1]
-        self.board[key[0]][key[1]] = value
+        self.board[key[0] + key[1] * 16] = value
 
     def __delitem__(self, key):
         value.position['x'] = None
         value.position['y'] = None
-        self.board[key[0]][key[1]] = None
+        self.board[key[0] + key[1] * 16] = None
