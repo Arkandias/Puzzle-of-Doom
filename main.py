af945761a5989d2d3abfee24fc02c12d0c27aecc
Kagaminara Shikatsu <misterarkandias@gmail.com>
2017-02-09 00:28:15 +0100

    Added some Arbiter checks tests

--- main.py
+++ main.py
@@ -3,15 +3,19 @@ from piece import Piece
 from piecesbank import PiecesBank
 from ui import UI
 from board import Board
+from arbiter import Arbiter
 
 def main():
 
     pb = PiecesBank()
     app = UI()
-    ### DO NOT FUCKING REMOVE THIS. I DARE YOU. ###
-    app.preloadPieces(pb.pieceslist)
+    arbiter = Arbiter()
     board = Board()
 
+    arbiter.isGoalAchieved(board)
+
+    ### DO NOT FUCKING REMOVE THIS. I DARE YOU. ###
+    app.preloadPieces(pb.pieceslist)
     ### DO NOT FUCKING REMOVE THIS EITHER. ###
     app.mainloop()
 
