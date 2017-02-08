f775533cd5656408502e095732000063dd995276
Tamer <lucas.hertzog@epitech.eu>
2017-02-08 21:04:47 +0100

    mon arbitre pour voir si une piece marche, marche, avec 3 pieces pour l'instant, j'ai pas test avec plus

--- arbiter.py
+++ arbiter.py
@@ -13,14 +13,29 @@ class arbiter():
 
     def isPlacmentValid(self, piece, board):
         nearbyPieces = []
-        nearbyPieces[0] = board[(piece.position.x), (piece.position.y <= 0 if NULL else piece.position.y - 1)]
-        nearbyPieces[1] = board[(piece.position.x >= 15 if NULL else piece.position.x + 1), (piece.position.y)]
-        nearbyPieces[2] = board[(piece.position.x), (piece.position.y >= 15 if NULL else piece.position.y + 1)]
-        nearbyPieces[3] = board[(piece.position.x <= 0 if NULL else piece.position.x - 1), (piece.position.y)]
+        if (piece.position['y'] <= 0):
+            nearbyPieces.append(None)
+        else:
+            nearbyPieces.append(board[(piece.position['x']), (piece.position['y'] - 1)])
+
+        if (piece.position['x'] >= 15):
+            nearbyPieces.append(None)
+        else:
+            nearbyPieces.append(board[(piece.position['x'] + 1), (piece.position['y'])])
+
+        if (piece.position['x'] >= 15):
+            nearbyPieces.append(None)
+        else:
+            nearbyPieces.append(board[(piece.position['x']), (piece.position['y'] + 1)])
+
+        if (piece.position['x'] <= 0):
+            nearbyPieces.append(None)
+        else:
+            nearbyPieces.append(board[(piece.position['x'] - 1), (piece.position['y'])])
 
         for i in range(0, 4):
             if (nearbyPieces[i] is not None):
-                if (numToEdge(nearbyPieces[i], (i + 2 + nearbyPieces.nbofrightrotate) % 4 != numToEdge(piece, (
-                                i + 4 - piece.nbofrightrotate) % 4))):
-                    return false
-        return true
+               if (self.numToEdge(nearbyPieces[i], (i + 2 + nearbyPieces[i].nbofrightrotate) % 4) != self.numToEdge(piece, (
+                                i + 4 - piece.nbofrightrotate) % 4)):
+                    return False
+        return True
