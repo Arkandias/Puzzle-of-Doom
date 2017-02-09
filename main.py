23c6775888a9ff49fd691c2b594f2edbfcef3f72
Kagaminara Shikatsu <misterarkandias@gmail.com>
2017-02-09 01:04:13 +0100

    Removed tests

--- main.py
+++ main.py
@@ -12,16 +12,6 @@ def main():
     arbiter = Arbiter()
     board = Board()
 
-    for y in range(16):
-        for x in range(16):
-            board[x, y] = pb.pieceslist[x + y*16]
-            board[x, y].leftEdge = PatternPieces.EDGE
-            board[x, y].rightEdge = PatternPieces.EDGE
-            board[x, y].upEdge = PatternPieces.EDGE
-            board[x, y].downEdge = PatternPieces.EDGE
-
-    print(arbiter.isGoalAchieved(board))
-
     ### DO NOT FUCKING REMOVE THIS. I DARE YOU. ###
     app.preloadPieces(pb.pieceslist)
     ### DO NOT FUCKING REMOVE THIS EITHER. ###
